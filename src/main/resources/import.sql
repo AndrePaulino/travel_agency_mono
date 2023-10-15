b978a55064298e320dee718e6e6dcace0c0317b1
André Paulino <andrepaulinobtw@gmail.com>
2023-10-15 02:57:59 -0300

    Create Flight Entity & Resource

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,11 @@ INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
 INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
 INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
 INSERT INTO TravelOrder(id) VALUES (nextval('travelorder_sequence'));
+
+create sequence flight_sequence start with 1 increment by 1;
+INSERT INTO Flight(id, travelOrderId, fromAirport, toAirport) VALUES (nextval('flight_sequence'), 1, 'GRU', 'MCO');
+INSERT INTO Flight(id, travelOrderId, fromAirport, toAirport) VALUES (nextval('flight_sequence'), 2, 'BSB', 'JFK');
+INSERT INTO Flight(id, travelOrderId, fromAirport, toAirport) VALUES (nextval('flight_sequence'), 3, 'LAX', 'ATL');
+INSERT INTO Flight(id, travelOrderId, fromAirport, toAirport) VALUES (nextval('flight_sequence'), 4, 'LHR', 'MEX');
+INSERT INTO Flight(id, travelOrderId, fromAirport, toAirport) VALUES (nextval('flight_sequence'), 5, 'GIG', 'BSB');
+
